3abafd197fba12e07209c2325ebf43a0719b3784
cpeucher <cpeucher@univ-rennes.fr>
2024-10-29 09:57:23 +0100

    Adapted elec_rise_time to new elec_filter function

--- src/electrical/elec_rise_time.m
+++ src/electrical/elec_rise_time.m
@@ -29,19 +29,23 @@ function sig = elec_rise_time(sig,rise_time)
 % -------------------------------------------------------------------------
 % GLOBAL:
 % -------------------------------------------------------------------------
-% 
+% frequency_array   relative frequency samples, in Hz [real vector]
 %
 % -------------------------------------------------------------------------
 % -------------------------------------------------------------------------
+
+global frequency_array
 
 rise_time_filter.type = 'gaussian';
 rise_time_filter.order = 1;
 rise_time_filter.f3dB = 0.3321/rise_time;
 % Relation between cut-off frequency and rise time for a 1st order Gaussian
-% electrical low-pass filter.       
-    
-sig = elec_elpf(sig,rise_time_filter);
-% Apply rise time filter.
+% electrical low-pass filter   
 
+tf = elec_tf_elpf(rise_time_filter,frequency_array);
+% Calculate Gaussian filter transfer function
+    
+sig = real(elec_filter(sig,tf));
+% Apply rise time filter
 
 end
