aae0c9f37b4eb663e310b165d262f1154522bbe1
cpeucher <cpeucher@univ-rennes.fr>
2025-04-08 12:42:44 +0200

    Added WGN samples addition function for discrete-time system

---
+++ src/digicoms/add_awgn.m
@@ -0,0 +1,57 @@
+function symbs = add_awgn(symbs,esn0)
+% Addition of complex AWGN to signal samples in the digital domain
+%
+% -------------------------------------------------------------------------
+% DESCRIPTION:
+% -------------------------------------------------------------------------
+% This function adds samples of (complex, complex circularly-symmetric)
+% white Gaussian noise to a discrete time signal according to a specified
+% signal-to-noise ratio, defined in terms of energy per symbol divided by 
+% % white noise spectral density, Es/N0.
+% The energy per symbol is calculated from the input signal samples. It is
+% therefore understood that the length of the input signal should be
+% sufficient for this calculation to make sense (i.e. the signal should
+% describe the entire constellation).
+%
+% -------------------------------------------------------------------------
+% FUNCTION CALL:
+% -------------------------------------------------------------------------
+% symbs = add_awgn(symbs,esn0); 
+%
+% -------------------------------------------------------------------------
+% INPUTS:
+% -------------------------------------------------------------------------
+% symbs             samples of the signal [real or complex vector]
+%                       The signal is assumed to be noiseless.
+%
+% esn0              target signal-to-noise ratio, in dB [real scalar]
+%                       The signal-to-noise ratio is expressed in terms of
+%                       energy per symbol divided by the additive white 
+%                       Gaussian noise spectral density, Es/N0.
+%
+% -------------------------------------------------------------------------
+% OUTPUTS:
+% -------------------------------------------------------------------------
+% symbs             samples of the noisy signal [complex vector]
+%
+% -------------------------------------------------------------------------
+% GLOBAL:
+% -------------------------------------------------------------------------
+% 
+%
+% -------------------------------------------------------------------------
+% -------------------------------------------------------------------------
+
+nsymbols = length(symbs);
+% Number of symbols
+
+es = mean(abs(symbs).^2);
+% Mean symbol energy
+
+n0 = es / 10^(esn0/10);
+% Noise spectral density
+
+symbs = symbs + sqrt(n0/2)*(randn(1,nsymbols)-1i*randn(1,nsymbols));
+% Noisy signal
+
+end
